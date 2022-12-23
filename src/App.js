7e0d4e38b87d6b324de3a67a5c6cf7e3c5c77432
Valentina Ruiz <v.zruiz06@gmail.com>
2022-12-23 07:01:13 -0800

    added axios call syntax

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // axios.get(`${process.env.REACT_APP_BACKEND_URL}/boards`, {
+
   return (
     <div className="App">
       <header className="App-header">
