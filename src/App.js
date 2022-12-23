27d33723a48c69195f25938cecbf77c20c3f5424
Laura Skomerza <lskomerza@gmail.com>
2022-12-23 12:06:22 -0800

    added boardslist and components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 import Board from "./components/Board";
 import BoardsList from "./components/BoardsList";
@@ -9,13 +9,24 @@ import NewBoardForm from "./components/NewBoardForm";
 // 3. Display list of Boards
 // 4. Form
 
-const boardsData = [];
+const boardsData = [
+  {
+    board_id: 1,
+    title: "Do things!",
+    owner: "Milena",
+  },
+  {
+    board_id: 2,
+    title: "You can do it!",
+    owner: "Laura",
+  },
+];
 
 function App() {
   return (
     <div className="App">
       {/* <Board /> */}
-      <BoardsList boardsList={boardsData}/>
+      <BoardsList boardsList={boardsData} />
       {/* <NewBoardForm /> */}
     </div>
   );
