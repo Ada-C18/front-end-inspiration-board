d09d729853b2d09c81bb68fc81b2952032687bc3
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2023-01-03 12:09:15 -0800

    Initial App Classnames and CSS grid layout

--- front-end-inspiration-board/src/App.js
+++ front-end-inspiration-board/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import background from "./cork-board.jpg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>INSPIRATION BOARD</h1>
       </header>
+      <body>
+        <section className="container">
+          <h2 className="boardSelector">Select an Existing Board </h2>
+          <h2 className="boardList">This is the list of boards will go</h2>
+          <h2 className="HeaderOfNewBoard">Create a New Board</h2>
+          <h2 className="CreateNewBoard">Form</h2>
+          <h2 className="CardsForSectedBoard">Cards for Selected Board</h2>
+          <button className="CreateNewCard">Create a New Card</button>
+        </section>
+      </body>
     </div>
   );
 }
