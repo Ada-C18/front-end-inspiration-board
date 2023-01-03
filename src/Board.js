c3256968a4105164f89e7e89fdd405fa3d83d568
samiya <samiyai96@gmail.com>
2023-01-03 15:01:48 -0500

    Updated componenets part 2

---
+++ src/Board.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Board;
---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Card;
