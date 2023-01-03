279fc2929fc826853bc01b995aa6633242660be9
Ariel <gabriellaw15@gmail.com>
2023-01-03 12:03:10 -0800

    Added titles to web page

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1 className="App-header"> Mindful Moments</h1>
-      <h2></h2>
+      <h2>BOARDS</h2>
+      {/* <main>
+        <BoardsList entries={singleBoard} />
+      </main> */}
+      <h2>SELECTED BOARDS</h2>
+      <h2>CREATE NEW BOARD</h2>
+      {/* <NewBoardForm></NewBoardForm> */}
+      <h2>CARDS FOR "selected board"</h2>
+      {/* <NewCardForm></NewCardForm> */}
+      <h2>CREATE NEW CARD</h2>
+      {/* <main>
+        <CardsList entries={singleCard} />
+      </main> */}
     </div>
   );
 }
