1b9b6d0a22f761d84cffcdfe61c96e8e37ded777
Svetlana <svetikyss89@gmail.com>
2023-01-04 06:47:35 -0800

    new Board Foarm component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from "./components/Board";
+import "./App.css";
+import NewBoardForm from "./components/NewBoardForm";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+// const BaseUrl = "http://localhost:5000";
 
 function App() {
+  const [boardsData, setBoardsData] = useState([]);
+
+  const createNewBoard = (newBoardData) => {
+    axios
+      .post("http://localhost:5000/boards", newBoardData)
+      .then((response) => {
+        const newBoards = [...boardsData];
+        // const newId = Math.max(...newBoards.map(board => board.id))+1
+        newBoards.push({
+          // id: newId,
+          board_id: response.data.id,
+          title: response.data.title,
+          owner: response.data.owner,
+          ...boardsData,
+        });
+        setBoardsData(newBoards);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NewBoardForm createNewBoard={createNewBoard} />
+        <Board>Board</Board>
       </header>
     </div>
   );
