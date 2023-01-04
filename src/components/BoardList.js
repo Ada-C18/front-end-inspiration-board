44a8b23de884e059e6eb93b5577681d40395ffaf
Jaime Mitchell <JaimeMitchell100@gmail.com>
2023-01-04 15:39:41 -0500

    just adding some proptypes to cardlist

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Card from './Card'
-const CardList = (props) => {
+const CardList = (props.cards) => {
+  const cards= props.
  return(<div>
     <h2>Card List</h2>
-    <Card></Card>
+    <section className='CardList'>{cards}</section>
   </div>)
 };
 
 export default CardList;
+
+<Cardlist
+key={card.id}
+id={card.id}
+message={card.message}
+// liked={card.liked}
+addHeart={props.addHeart}
+/>
