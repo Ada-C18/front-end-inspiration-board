55aeef0b5abdc56a9bc486a4cb4e379c79fff081
Sarah Sanborn <sanbornsarahn@gmail.com>
2023-01-04 10:39:34 -0800

    Wrote App component return statement with JSX structure

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import Board from './components/Board.js';
 import CardList from './components/CardList.js';
 import NewBoardForm from './components/NewBoardForm';
+import NewCardForm from './components/NewCardForm';
 
 
 function App() {
   return (
     <body>
       <header>
-
+        <section>
+          <h1>SELECTED BOARD</h1>
+          <p>Owner Name</p>
+        </section>
+        <section>
+          <h2>Create A New Card</h2>
+          <NewCardForm></NewCardForm>
+        </section>
       </header>
-
-      <div>
-
-      </div>
-      
+      <aside>
+        <section>
+          <h1>Create A New Board</h1>
+          <NewBoardForm></NewBoardForm>
+        </section>
+        <section>
+          <h1>Boards</h1>
+          <Board></Board>
+        </section>
+      </aside>
       <main>
-
-
+        <div>
+          <h1>Cards for Affirmations</h1>
+          <CardList></CardList>
+        </div>
       </main>
     </body>
-   
-
-    
-
-  
   );
-}
+};
 
 export default App;
