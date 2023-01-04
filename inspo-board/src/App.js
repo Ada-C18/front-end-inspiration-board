8c2b448285c28126026b7b363651828fc5813231
Sierra <sierra.r.jordan@gmail.com>
2023-01-04 11:07:45 -0800

    adds getAllBoards

--- inspo-board/src/App.js
+++ inspo-board/src/App.js
@@ -1,11 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { useState } from "react";
-import boardData from "..boardData.json";
+import React, { useEffect, useState } from "react";
+// import boardData from "..boardData.json";
 import BoardList from "./components/boardList";
+import axios from "axios";
 
 function App() {
   const [boards, setBoards] = useState([]);
+
+  const URL = "http://127.0.0.1:5000/boards";
+
+  const getAllBoards = () => {
+    axios
+      .get(URL)
+      .then((response) => {
+        setBoards(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getAllBoards();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
