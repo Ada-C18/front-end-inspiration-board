9c11b80099102a161c024042e24cd3264b01edf8
Jaime Mitchell <JaimeMitchell100@gmail.com>
2023-01-04 16:26:58 -0500

    added Aretta's board

--- src/components/BoardForm.js
+++ src/components/BoardForm.js
@@ -4,17 +4,31 @@ const boardObj = {
   owner: '',
 };
 const BoardForm = (props) => {
-  const [formData, setFormData] = useState(boardObj)
+  const [formData, setFormData] = useState(boardObj);
+  const handleChange = (event) => {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    const newFormData = {...formData, [fieldName]: fieldValue}
+    setFormData(newFormData);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmitBoardForm(formData);
+    setFormData(boardObj);
+  }
   return(<div>
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Board Form</h2>
-      <input></input>
-      <input></input>
-      
-      <input>Submit</input>
-      <input></input>
+      <div>
+        <label htmlFor="title">Title: </label>
+        <input type="text" id="title" name="title" value={formData.title} onChange={handleChange}></input>
+      </div>
+      <div>
+        <label htmlFor="owner">Owner: </label>
+        <input type="text" id="owner" name="owner" value={formData.owner} onChange={handleChange}></input>
+      </div>
+      <div><input type="submit" value="Submit"></input></div>
     </form>
   </div>)
 }
-
 export default BoardForm
