3b55f90c78fda85bd9a23cdba87d42ea13441cbc
samiya <samiyai96@gmail.com>
2023-01-05 13:02:57 -0500

    Get new card almost done.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 // import Board from './componenets/Board';
 // import Card from './componenets/Card';
 // import CardList from './componenets/CardList';
-// import NewCard from './componenets/NewCard';
+import NewCard from './components/NewCard';
 // import NewBoard from './componenets/NewBoard';
 // import { useState } from 'react';
 
 
 function App() {
+  const handleCardSubmit=(data)=>{
+    //call api, updating the new cards on the board
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +20,7 @@ function App() {
         <div>
           <h2>Create New Board</h2>
           <h2>Create New Card</h2>
+          <NewCard handleCardSubmit={handleCardSubmit}/>
           <h2>Boards</h2>
         </div>
         
--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -1,20 +1,29 @@
 import { useState } from 'react';
 
-const NewCard = () => {
+const NewCard = ({handleCardSubmit}) => {
     const [message, setMessage] = useState('');
+    
     const onMessageChange = (event) => {
         setMessage(event.target.value)
+    };
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        handleCardSubmit=(message);
+        setMessage=('');
+        // do something with data
     }
-
+    //console.log('message',message);
     return (
-        <div className="new-card">
-            <h2>Create a New Card</h2>
-            <form className="new-card-form">
+        //<div className="new-card">
+            //<h2>Create a New Card</h2>
+            //form="new-card-form">
+            <form>
                 <label>New Card</label>
                 <input type="text" value={message} onChange={onMessageChange}/>
+                <div><input type="submit" value="message"/></div>
             </form>
-        </div>
+        //</div>
     )
 }
-
+//button add <extrernal callback
 export default NewCard;
