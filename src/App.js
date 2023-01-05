bac593ec15fe397ae1978a4f74bbf13bbe84ef07
Marilyn Thai <marilynthai15@gmail.com>
2023-01-05 11:25:05 -0800

    connected front to back-end

--- src/App.js
+++ src/App.js
@@ -1,47 +1,62 @@
-
-import './App.css';
-import { useState } from 'react';
-import BoardInfo from './components/BoardInfo';
-
+import "./App.css";
+import { useState } from "react";
+import BoardInfo from "./components/BoardInfo";
+import axios from "axios";
 
 function App() {
   const cardList = [
-    { 
-    id:1,
-    title: 'Great day',
-    message: 'Have an awesome today',
-    owner: 'Jhon Smith'
+    {
+      id: 1,
+      title: "Great day",
+      message: "Have an awesome today",
+      owner: "Jhon Smith",
     },
 
-    { 
-    id:2,
-    title: 'Nice',
-    message: 'Enjoy your day',
-    owner: 'Mary P'
+    {
+      id: 2,
+      title: "Nice",
+      message: "Enjoy your day",
+      owner: "Mary P",
     },
 
-    { 
-    id:3,
-    title: 'Courage',
-    message: 'You can do it',
-    owner: 'Nike'
+    {
+      id: 3,
+      title: "Courage",
+      message: "You can do it",
+      owner: "Nike",
     },
 
-    { 
-    id:4,
-    title: 'Blue Day',
-    message: 'Laugh is a great medicine',
-    owner: 'Emma Smith'
+    {
+      id: 4,
+      title: "Blue Day",
+      message: "Laugh is a great medicine",
+      owner: "Emma Smith",
     },
-];
+  ];
+  const [BoardList, setBoardList] = useState([]);
+
+  const URL = "http://127.0.0.1:5000";
+
+  const getAllBoards = () => {
+    axios
+      .get(`${URL}/boards`)
+      .then((response) => {
+        const boardsAPIResponseCopy = response.data;
+        console.log(response);
+        setBoardList(boardsAPIResponseCopy);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const firstCopy = cardList.map((card) => {
-    return {...card};
-    } );
+    return { ...card };
+  });
 
   const [entries, setEntries] = useState(firstCopy);
 
-  const deleteCard = id => {
+  const deleteCard = (id) => {
     console.log("delete Card");
     const newCardList = [];
     for (const card of entries) {
@@ -49,23 +64,16 @@ function App() {
         newCardList.push(card);
       }
     }
-    setEntries(newCardList)
+    setEntries(newCardList);
   };
-  
 
-  
   return (
     <div className="App">
       <h1> Inspiration Board </h1>
 
       <h2> Create a New Board</h2>
-
-
-      <BoardInfo 
-            entries={entries} 
-            deleteCard={deleteCard} 
-            />
-
+      {getAllBoards()}
+      <BoardInfo entries={entries} deleteCard={deleteCard} />
     </div>
   );
 }
--- src/components/BoardInfo.js
+++ src/components/BoardInfo.js
@@ -1,38 +1,34 @@
-
 import Card from "./Card";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const BoardInfo = ({entries, deleteCard}) => {
-    const cardComponents = entries.map((card) => {
-        return (
-            <div key={card.id}>
-                <Card
-                        id={card.id}
-                        title={card.title}
-                        owner={card.owner}
-                        message={card.message} 
-                        deleteCard={deleteCard}
-                        />  
-            </div>
-        )
-    })
-
+const BoardInfo = ({ entries, deleteCard }) => {
+  const cardComponents = entries.map((card) => {
     return (
-        <div>
-            {cardComponents}
-        </div>
-        );
+      <div key={card.id}>
+        <Card
+          id={card.id}
+          title={card.title}
+          owner={card.owner}
+          message={card.message}
+          deleteCard={deleteCard}
+        />
+      </div>
+    );
+  });
+
+  return <div>{cardComponents}</div>;
 };
 
-BoardInfo.propTypes ={
-    entries : PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            title: PropTypes.string.isRequired,
-            owner: PropTypes.string.isRequired,
-            message: PropTypes.string.isRequired,
-        })),
-    deleteCard: PropTypes.func.isRequired,
-}
+BoardInfo.propTypes = {
+  entries: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      owner: PropTypes.string.isRequired,
+      message: PropTypes.string.isRequired,
+    })
+  ),
+  deleteCard: PropTypes.func.isRequired,
+};
 
 export default BoardInfo;
