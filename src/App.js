9a4d59b8044cc18c096dd1e752addde482fc74d5
Nicole Mejia <snicolemejia@gmail.com>
2023-01-05 11:00:15 -0500

    added new board form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import './App.css';
+
+import { useState } from 'react';
+import axios from 'axios';
+
 import BoardContainer from './components/BoardContainer';
 import CardContainer from './components/CardContainer';
+import NewBoardForm from './components/NewBoardForm';
 
 
 function App() {
+  const [boards, setBoards] = useState([]);
+
+
+  // create new board, post request
+  const addBoard = (boardData) => {
+    axios
+      .post(URL, boardData)
+      .then((response) => {
+        const newBoards = [...boards];
+        newBoards.push({ title: response.data.title, owner: response.data.owner, ...boardData });
+        setBoards(newBoards);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="App">
+      <NewBoardForm addBoardCallback={addBoard}/>
       {/* <BoardContainer boards={boardData} />
       <CardContainer cards={cardData} /> */}
     </div>
---
+++ src/components/NewBoardForm.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useState } from 'react';
+import './NewBoardForm.css';
+
+// props = event handler for submit button
+const NewBoardForm = ({ addBoardCallback }) => {
+  // handle event on click to make post request to backend using axios
+  // hide form button - use ternary
+
+  const [boardData, setBoardData] = useState({
+    title: '',
+    owner: '',
+  });
+
+  const submitBoardData = (event) => {
+    event.preventDefault();
+
+    addBoardCallback(boardData);
+    setBoardData({ title: '', owner: ''});
+  };
+
+  const handleChange = (event) => {
+    setBoardData({ ...boardData, [event.target.name]: event.target.value });
+  };
+  
+  
+  return (
+    <form onSubmit={submitBoardData} className="new-board__form">
+      <section>
+        <h2>Add a New Board</h2>
+        <div className="new-board__fields">
+          <label htmlFor="title">Title</label>
+          <input
+            name="title"
+            id="title"
+            value={boardData.title}
+            onChange={handleChange}
+          />
+          <label htmlFor="owner">Owner</label>
+          <input
+            name="owner"
+            id="owner"
+            value={boardData.owner}
+            onChange={handleChange}
+          />
+          <button className="button new-board__submit" type="submit">
+            Add Board
+          </button>
+        </div>
+      </section>
+    </form>
+  );
+};
+
+export default NewBoardForm;
