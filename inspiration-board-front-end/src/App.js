4c6a9ed0a1548a2923dd981d7339853943dcb15f
misha-joy <mishellajoy@gmail.com>
2023-01-06 10:14:57 -1000

    gets all boards from backend

--- inspiration-board-front-end/src/App.js
+++ inspiration-board-front-end/src/App.js
@@ -2,18 +2,51 @@ import "./App.css";
 import BoardList from "./components/BoardList";
 import CardList from "./components/CardList";
 import NewBoardForm from "./components/NewBoardForm";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const testBoards = [
-  { title: "Test Board One ", id: 1, owner: "Pavi" },
-  { title: "Test Board Two", id: 2, owner: "Soleil" },
-  { title: "Test Board Three", id: 3, owner: "Misha" },
-];
+export const URL = "https://powerful-mesa-70650.herokuapp.com/boards";
+
+const convertFromApi = (apiBoard) => {
+  const { title, owner, id } = apiBoard;
+  const newBoard = { id, title, owner };
+  return newBoard;
+};
+
+const getAllBoards = async () => {
+  try {
+    const response = await axios.get(URL);
+    console.log(response.data);
+    return response.data.map(convertFromApi);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+// const testBoards = [
+//   { title: "Test Board One ", id: 1, owner: "Pavi" },
+//   { title: "Test Board Two", id: 2, owner: "Soleil" },
+//   { title: "Test Board Three", id: 3, owner: "Misha" },
+// ];
 const emptyBoard = { id: 0, title: " ", owner: " " };
 
 function App() {
-  const [boardData, setBoardData] = useState(testBoards);
+  const [boardData, setBoardData] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState(emptyBoard);
+
+  const refreshBoard = async () => {
+    try {
+      const boards = await getAllBoards();
+      setBoardData(boards);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    refreshBoard();
+  }, []);
+
   const clickToSelectBoard = (id) => {
     console.log("this function is running!");
     for (const board of boardData) {
