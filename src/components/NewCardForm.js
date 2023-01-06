517bd2c75f736fb5ba2f4e9928f920aaf8933493
Sarah Sanborn <sanbornsarahn@gmail.com>
2023-01-06 11:32:18 -0800

    Updated NewCardForm component to incorporate useState and a preview of what user types

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,17 +1,46 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-// need to add useState to sync the input and preview
-const NewCardForm = () => {
+const NewCardForm = ({createNewCardForm}) => {
+  const [formData, setFormData] = useState({
+    message: ''
+  });
+
+  const [preview, setPreview] = useState('');
+
+  const handleChange = (event) => {
+    const newFormData = {
+      ...formData,
+      [event.target.name]: event.target.value,
+    };
+    setFormData(newFormData);
+    setPreview(`${newFormData.message}`)
+  };
+
   return(
     <form>
-      <label>Message</label>
-      <input></input>
+      <div>
+        <label htmlFor='message'>Message</label>
+        <input
+          type='text'
+          id='message'
+          name='message'
+          value={formData.message}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <p>Preview: {preview}</p>
+      </div>
 
       <input type="submit" value="Submit"></input>
-
     </form>
-
   );
 
+};
+
+NewCardForm.propTypes = {
+  createNewCardForm: PropTypes.func.isRequired
 };
 
 export default NewCardForm;
