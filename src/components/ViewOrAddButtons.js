3af26349035b97365225b701a861d68029fa4532
Evelyn <hay.evelyn@gmail.com>
2023-01-07 09:06:56 -0800

    Added ViewOrAddButtons. Styled. Buttons need functionality added and styling after events

---
+++ src/components/ViewOrAddButtons.js
@@ -0,0 +1,13 @@
+import "./ViewOrAddButtons.css"
+//Still needs functionality
+
+const ViewOrAddButtons = () => {
+    return (
+        <div className="button">
+            <button>View All Boards</button>
+            <button>Create New Board</button>
+        </div>
+    );
+};
+
+export default ViewOrAddButtons;
