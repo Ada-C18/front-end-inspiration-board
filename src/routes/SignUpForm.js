1caa61bb6e246eef4c310662b52cf020fbab39b7
Katherine Abrikian <kabrikian@gmail.com>
2023-01-15 07:43:21 -0500

    basic set up of sign up form and css complete. small edit to login form

--- src/routes/SignUpForm.js
+++ src/routes/SignUpForm.js
@@ -1,13 +1,65 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 import "./SignUpForm.css";
 
+const kDefaultFormState = {
+  name: "",
+  password: "",
+};
+
 const SignUpForm = () => {
+  const handleLogIn = useLoaderData();
+  const [formData, setFormData] = useState(kDefaultFormState);
+
+  const handleChange = (event) => {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    const newFormData = { ...formData, [fieldName]: fieldValue };
+
+    setFormData(newFormData);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleLogIn(formData);
+    setFormData(kDefaultFormState);
+  };
+
   return (
     <>
-      <p>I am the sign up form</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Username</label>
+          <input
+            className="entry"
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        {/* <div>
+          <label htmlFor="password">Password</label>
+          <input
+            className="entry"
+            type="text"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div> */}
+        <div>
+          <input id="submit" type="submit" value="Sign Up!" />
+        </div>
+      </form>
       <p>
-        Already have an account? <Link to={`/login`}>Log in</Link>
+        Already have an account?{" "}
+        <Link id="loginLink" to={`/login`}>
+          Log in
+        </Link>
       </p>
     </>
   );
