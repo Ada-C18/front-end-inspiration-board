830d4f9e83c50a48a1b9d1f4df31c24f8ece8aef
Anna <annalordkemp@gmail.com>
2023-01-16 12:13:09 -0700

    complete styling on AddCard and add shadow around home button

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -2,7 +2,22 @@ import './AddCard.css';
 
 const AddCard = () => {
   return (
-    <p> </p>
+    <div>
+      <div id='card-preview'>
+        Add a new card to this board!
+      </div>
+      <form id='new-card-form'>
+        <div>
+          <input type='text' value="Enter your card's message here!" id='new-card-text'/>
+        </div>
+        <div>
+          <input type='submit' value='Submit' id='new-card-submit' />
+        </div>
+      </form>
+
+
+
+    </div>
   )
 }
 
