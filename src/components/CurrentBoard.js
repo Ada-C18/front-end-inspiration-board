062f0195d848bcb1aa2162ccd19f2d83d7925dd2
Kae Sluder <kaesluder@fastmail.com>
2023-01-16 13:51:01 -0500

    css restyling

--- src/components/CurrentBoard.js
+++ src/components/CurrentBoard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import './CurrentBoard.css';
+import './CardList.css';
 
 const CurrentBoard = function (props) {
   console.log(props.currentBoardState);
