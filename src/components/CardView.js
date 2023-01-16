4caf50d9fdba43f370d035ea7a3830edd7c87f1a
Anna <annalordkemp@gmail.com>
2023-01-16 14:58:39 -0700

    adds styling for hovering over like and delete buttons, adds state to AddCard component to update the preview card as user types in new card message

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -4,9 +4,9 @@ const CardView = (props) => {
   return (
     <div className={`single-card ${props.shadowClass}`}> 
       <p id='card-message'> {props.message} </p> 
-      <span className='likes-and-delete'> {props.likes} ♥️ </span>
-      <button className='likes-and-delete'> +1 </button>
-      <button className='likes-and-delete'> Delete </button>
+      <span id='num-likes'> {props.likes} ♥️ </span>
+      <button className='like-and-delete-buttons'> +1 </button>
+      <button className='like-and-delete-buttons'> Delete </button>
     </div>
   )
 };
