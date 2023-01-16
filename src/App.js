543645a65fb0e866adf87767df8c5fcb2003f566
Eve Stroud <evestroud89@gmail.com>
2023-01-16 00:02:54 -0500

    Implement partial board selector functionality

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Header from "./Components/Header";
 import Board from "./Components/Board";
 
 const BACKEND_URL = `${process.env.REACT_APP_BACKEND_URL}`;
 
 function App() {
-  const [listOfBoards, setListOfBoards] = useState(/* TODO */);
+  const [listOfBoards, setListOfBoards] = useState([]);
   const addNewBoard = () => {
     /* TODO */
   };
-
-  const [currentBoard, setCurrentBoard] = useState(/* TODO */);
-  const updateCurrentBoard = () => {
-    /* TODO */
+  const getBoardList = () => {
+    axios
+      .get(`${BACKEND_URL}/boards`)
+      .then((result) => {
+        setListOfBoards(result.data);
+      });
   };
+
+  const [currentBoard, setCurrentBoard] = useState(null);
+  const updateCurrentBoard = (id) => {
+    setCurrentBoard(id ? parseInt(id) : null)
+  };
+
+  useEffect(() => getBoardList, []
+    
+  )
 
   return (
     <div className="App">
--- src/Components/BoardSelector.js
+++ src/Components/BoardSelector.js
@@ -1,5 +1,18 @@
 const BoardSelector = (props) => {
-  return "";
+  let options = props.listOfBoards.map((board) => {
+    return <option key={board.id} value={board.id}>{board.name}</option>;
+  });
+
+  const onSelectBoard = e => {
+    props.updateCurrentBoard(e.target.value);
+  }
+
+  return (
+      <select name="boardSelector" onChange={onSelectBoard}>
+        <option value="">Select Board:</option>
+        {options}
+      </select>
+  );
 };
 
 export default BoardSelector;
