baeaeebf75dc3de8b833328502179d78c13164e5
hannahtan96 <hannahtan96@gmail.com>
2023-01-17 12:12:42 -0800

    connected card to board on the front end, but is not immediately rendering

--- inspo-board/src/App.js
+++ inspo-board/src/App.js
@@ -1,23 +1,26 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { useEffect, useState } from "react";
-import BoardList from "./components/BoardList";
-import axios from "axios";
-import NewBoardForm from "./components/NewBoardForm";
-import CardList from "./components/CardList";
+import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import BoardList from './components/BoardList';
+import axios from 'axios';
+import NewBoardForm from './components/NewBoardForm';
+import CardList from './components/CardList';
+import NewCardForm from './components/NewCardForm';
+import card from './components/Card';
 
 function App() {
   const [boards, setBoards] = useState([]);
-  const [selectedBoard, setSelectedBoard] = useState("");
+  const [selectedBoard, setSelectedBoard] = useState('');
   const [showForm, setShowForm] = useState(true);
   const [showCard, setShowCard] = useState(true);
   const [cards, setCards] = useState([]);
 
-  const URL = "http://127.0.0.1:5000/boards";
+  const BOARD_URL = 'http://127.0.0.1:5000/boards';
+  const CARD_URL = 'http://127.0.0.1:5000/cards';
 
   const getAllBoards = () => {
     axios
-      .get(URL)
+      .get(BOARD_URL)
       .then((response) => {
         setBoards(response.data);
       })
@@ -34,7 +37,7 @@ function App() {
     const board = { board_id, title, owner, cards };
     setSelectedBoard(board);
     axios
-      .get(`${URL}/${board_id}`)
+      .get(`${BOARD_URL}/${board_id}`)
       .then((result) => {
         console.log(result.data);
         setCards(result.data.cards);
@@ -46,13 +49,13 @@ function App() {
 
   const addNewBoard = (newBoard) => {
     axios
-      .post(URL, newBoard)
+      .post(BOARD_URL, newBoard)
       .then((result) => {
         console.log(result.data);
         const newBoardData = {
           board_id: result.data.board.board_id,
           title: result.data.board.title,
-          owner: result.data.board.owner,
+          owner: result.data.board.owner
         };
         setBoards([...boards, newBoardData]);
       })
@@ -61,9 +64,40 @@ function App() {
       });
   };
 
+  const addNewCard = (newCard) => {
+    console.log(newCard);
+    axios
+      .post(CARD_URL, newCard)
+      .then((result) => {
+        const newCardData = {
+          board_id: selectedBoard.board_id,
+          card_id: result.data.card.card_id,
+          message: result.data.card.message
+        };
+        // new function
+        connectCardToBoard(newCardData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const connectCardToBoard = (cardData) => {
+    console.log(cardData);
+    axios
+      .patch(`${BOARD_URL}/${cardData.board_id}/card`, cardData)
+      .then((result) => {
+        console.log(result.data);
+        setCards([...cards, result.data]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <h1>Inspiration Board</h1>
       </header>
       <main>
@@ -74,17 +108,24 @@ function App() {
           <h4>
             {selectedBoard
               ? `${selectedBoard.title} - ${selectedBoard.owner}`
-              : "Select a board from the list"}
+              : 'Select a board from the list'}
           </h4>
         </div>
         <h2>Create a new board</h2>
-        {showForm ? <NewBoardForm addNewBoardCallback={addNewBoard} /> : ""}
+        {showForm ? <NewBoardForm addNewBoardCallback={addNewBoard} /> : ''}
         <button onClick={() => setShowForm(!showForm)}>
-          {showForm ? "Hide new board form" : "Show new board form"}
+          {showForm ? 'Hide new board form' : 'Show new board form'}
         </button>
         <div>
           <h2>Cards</h2>
-          {selectedBoard ? <CardList cards={selectedBoard.cards} /> : ""}
+          {selectedBoard ? (
+            <div>
+              <CardList cards={selectedBoard.cards} />
+              {showCard ? <NewCardForm addNewCardCallback={addNewCard} /> : ''}
+            </div>
+          ) : (
+            ''
+          )}
         </div>
       </main>
     </div>
---
+++ inspo-board/src/components/NewCardForm.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NewCardForm = (props) => {
+  const [formData, setFormData] = useState({ message: '' });
+
+  const handleChange = (e) => {
+    // const NewFormData = { ...formData, [e.target.name]: e.target.value };
+    setFormData({ message: e.target.value });
+  };
+
+  const handleNewCardSubmit = (e) => {
+    e.preventDefault();
+    props.addNewCardCallback(formData);
+    setFormData({ message: '' });
+  };
+
+  return (
+    <form onSubmit={handleNewCardSubmit}>
+      <label htmlFor='message'>Message</label>
+      <input
+        type='text'
+        id='message'
+        name='message'
+        value={formData.message}
+        onChange={handleChange}
+      />
+      <p>Preview: {formData.message}</p>
+      <input type='submit' value='Add card' />
+    </form>
+  );
+};
+
+NewCardForm.propTypes = {
+  addNewCardCallback: PropTypes.func.isRequired
+};
+
+export default NewCardForm;
