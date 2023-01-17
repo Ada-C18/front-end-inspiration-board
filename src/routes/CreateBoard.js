0388ee84d173fd4a882cfa30540293d7697704a1
Anna <annalordkemp@gmail.com>
2023-01-17 16:32:17 -0700

    completes basic styling for CreateBoard and links to home

--- src/routes/CreateBoard.js
+++ src/routes/CreateBoard.js
@@ -1,8 +1,33 @@
+import { Link } from 'react-router-dom';
 import './CreateBoard.css';
+import homeIconWhite from '../home-icon-white.png';
 
 const CreateBoard = () => {
   return (
-    <p>This is where you'll create a new board. Maybe I'll end up on a different view!</p>
+    <div id='new-board-view'>
+
+      <h1 id='create-board-heading'>Create a new board</h1>
+      
+      <form id='new-board-form'>
+        <div>
+          <input type='text' value='Board title' id='board-title-field'></input>
+        </div>
+        {/* <div>
+          <input type='text' value='Your name (board owner)'></input>
+        </div> */}
+        <div>
+          <input type='submit' value='Submit New Board!' id='submit-button'></input>
+        </div>
+      </form>
+      
+
+      <Link to={`/boards`}>
+            <button id='home-button-white'> 
+              <img src={homeIconWhite} alt='home button' id='button-img'/>
+            </button>
+      </Link>
+
+    </div>
   )
 };
 
