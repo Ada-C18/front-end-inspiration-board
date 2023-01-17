f8dee8fb3d8d9b4f6e433ce9ae6065dbfcab132e
Aisha Husseini <ayasha.husseini@gmail.com>
2023-01-17 10:41:01 -0500

    added deployed backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import NewBoardForm from './components/NewBoardForm';
+import NewCardForm from './components/NewCardForm';
+import Board from './components/Board';
+import CardsList from './components/CardsList';
+import Card from './components/Card';
 
 function App() {
+
+  // const [boardsData, setBoardsData] = useState([]);
+  // const [selectedBoard, setSelectedBoard] = useState({
+  //   title: '',
+  //   owner: '',
+  //   board_id: null
+  // });
+
+  // useEffect(() => {
+  //   axios.get("https://lucky7-inspiration-board-api.herokuapp.com/boards")}, {
+  //   }).then((response) => {
+  //     setBoardsData(response.data);
+  //   }, []);
+
+  // const selectBoard = (board) => { setSelectedBoard(board) };
+
+  // const boardsElements = boardsData.map((board) => {
+  //   return (<li>
+  //     <Board board={board} onBoardSelect={selectBoard}></Board>
+  //   </li>)
+  // });
+
+
+
+
   const boardData = {
     board_id: 92,
     owner: "Ada L.",
@@ -15,6 +47,7 @@ function App() {
 
   const newBoardData = {board_id: null, owner: "", title: ""};
 
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,16 +58,17 @@ function App() {
           <div className="selectedBoard">
             <h2>{boardData["title"]}</h2> 
           </div>
+
           <div className ="cardsContainer">
             <p className = "cardMessage">
       
               {cardData[0]["message"]}
-
+            
+              <CardsList>{CardsList}</CardsList>
             </p>
             <p className = "cardMessage">
               {cardData[1]["message"]}
             </p>
-            {/* <img className="likeButton" src="./assets/heart.png" alt="Like Button"></img> */}
           </div>
         
         <section className = "createNew">
@@ -42,13 +76,17 @@ function App() {
             
             <p className = "createBoard">
               <h2>Create New Board</h2>
+              <NewBoardForm></NewBoardForm>
             </p>
+            
           
           </div>
           <div className="newCardContainer">
-            
+
             <p className = "newCardBoard">
-              <h2>Create New Board</h2>
+              <h2>Create New Card</h2>
+              <NewCardForm>{NewCardForm}</NewCardForm>
+      
             </p>
           
           </div>
