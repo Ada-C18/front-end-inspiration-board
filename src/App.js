499fe11ac25fa5d9a47dc4b17bdc5bf3e4734824
Liat <liathirsh@gmail.com>
2023-01-17 14:14:35 -0500

    Mostly working

--- src/App.js
+++ src/App.js
@@ -4,161 +4,100 @@ import NewCardForm from "./components/NewCardForm";
 import Board from "./components/Board";
 import Card from "./components/Card";
 import CardList from "./components/CardList";
-import { useState } from "react";
-const URL = process.env[' REACT_APP_BACKEND_URL'];
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-// const App = () => {
-//   const [boardData, setBoardData] = useState([]);
-//   const [cardData, setCardData] = useState([]);
-//   useEffect(() => {   
-//     axios
-//       .get(URL)
-//       .then((response)=>{
-//         const newBoards = response.data.map((board)=> {
-//           return{
-//             id: board.id,
-//             title: board.title,
-//             owner: board.owner
-//           };
-//         });
-//         setBoardData(newBoards);
-//     })
-//     .catch((error)=> {
-//       console.log(error);
-//     });
-//     },[]);
+//const URL = process.env['REACT_APP_BACKEND_URL'];
+const URL = "http://localhost:5000/boards";
 
-//     const addBoard = (board) => {
-//       axios
-//         .post(URL, boardData)
-//         .then((response) => {
-//           const newBoards = [...boardData];
-//           newBoards.push({ id: response.data.id, title:'', ...board});
-//           setBoardData(newBoards);
-//         })
-//         .catch((error)=> console.log (error));
-
-//     };
-//     useEffect(() => {   
-//       axios
-//         .get(URL)
-//         .then((response)=>{
-//           const newCards = response.data.cards.map((card)=> {
-//             return{
-//               id: card.id,
-//               message: card.message
-//             };
-//           });
-//           setCardData(newCards);
-//       })
-//       .catch((error)=> {
-//         console.log(error);
-//       });
-//       },[]);
-
-//       const addCard = (card) => {
-//         axios
-//           .post(URL, cardData)
-//           .then((response) => {
-//             const newCards = [...cardData];
-//             newCards.push({ id: response.data.id, message:'', ...card});
-//             setBoardData(newCards);
-//           })
-//           .catch((error)=> console.log (error));
-//       };
-
-//       const deletCard = (id) => {
-//         axios 
-//           .delete(`${URL}/${id}`)
-//           .then(()=> {
-//             const newCards = cardData.filter((card)=> card.id !==id);
-//             setCardData(newCards);
-//           })
-//           .catch((error)=> {
-//             console.log(error);
-//           });
-//       };
-
-const boardsData = [
-  {
-    id: 1,
-    title: "hello world",
-    owner: "123",
-    boardClicked: false,
-    cards: [
-      { id: 1, message: "Today's best card" },
-      {
-        id: 2,
-        message: " Having a good time",
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: "hello east coast",
-    owner: "456",
-    cards: [
-      {
-        id: 3,
-        message: "Friday vibes",
-      },
-    ],
-  },
-];
-
-function App() {
-  const [boardData, setBoardData] = useState(boardsData);
-  const [cardData, setCardData] = useState(boardData.cards);
-
-  const displayCards = boardData.map((board) => {
-    return board.cards.message;
-  });
-
-  const updateBoardData = (updatedBoard) => {
-    const boards = boardData.map((board) => {
-      if (board.id === updatedBoard.id) {
-        return updatedBoard;
-      } else {
-        return board;
-      }
-    });
-    setBoardData(boards);
-  };
-  const updateCardData = (updatedCard) => {
-    const cards = cardData.map((card) => {
-      if (card.id === updatedCard.id) {
-        return updatedCard;
-      } else {
-        return card;
-      }
-    });
-    setCardData(cards);
-  };
-
+const App = () => {
+  const [boardData, setBoardData] = useState([]);
+  const [cardData, setCardData] = useState([]);
+  const [showForm, setShowForm] = useState(true);
   const [selectedBoard, setSelectedBoard] = useState({
     title: "",
     owner: "",
     id: null,
   });
 
-  const handleBoardClicked = (id) => {
-    const findId = boardData.map((board) => {
-      if (id === board.id) {
-        setSelectedBoard(board);
-        setCardData(board.cards);
-      }
+  //works
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        const newBoards = response.data.map((board) => {
+          return {
+            id: board.id,
+            title: board.title,
+            owner: board.owner,
+          };
+        });
+        setBoardData(newBoards);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-      // filter the cards based on id parameter
-      //another piece of state for selected cards
-    });
+  //works
+  const addBoard = (board) => {
+    axios
+      .post(URL, board)
+      .then((response) => {
+        const newBoards = [...boardData];
+        newBoards.push({ id: response.data.board_id, title: "", ...board });
+        setBoardData(newBoards);
+      })
+      .catch((error) => console.log(error));
   };
 
-  const [showForm, setShowForm] = useState(true);
+  const handleBoardClicked = (id) => {
+    console.log(id);
+    console.log(boardData);
+    const board = boardData.find((board) => {
+      return id === board.id;
+    });
+    if (!board) {
+      return;
+    }
+    axios
+      .get(`${URL}/${board.id}/cards`)
+      .then((response) => {
+        const newCards = response.data.cards.map((card) => {
+          return {
+            id: card.id,
+            message: card.message,
+          };
+        });
+        setCardData(newCards);
+        setSelectedBoard(board);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const addCard = (card) => {
+    axios
+      .post(URL, cardData)
+      .then((response) => {
+        const newCards = [...cardData];
+        newCards.push({ id: response.data.id, message: "", ...card });
+        setBoardData(newCards);
+      })
+      .catch((error) => console.log(error));
+  };
 
   const deleteCard = (id) => {
-    const newCards = cardData.filter((card) => card.id !== id);
-    setCardData(newCards);
-    console.log("i am deletCard");
+    axios
+      .delete(`${URL}/${id}`)
+      .then(() => {
+        const newCards = cardData.filter((card) => card.id !== id);
+        setCardData(newCards);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
@@ -179,7 +118,7 @@ function App() {
         </div>
         <div>
           <h1> Create A New Board </h1>
-          {showForm && <NewBoardForm onUpdateBoardData={updateBoardData} />}
+          {showForm && <NewBoardForm onUpdateBoardData={addBoard} />}
           <button type="button" onClick={() => setShowForm(!showForm)}>
             {" "}
             {showForm === true ? "Hide New Board Form" : "Show New Board Form"}
@@ -194,12 +133,13 @@ function App() {
         )}
         {selectedBoard.id && (
           <div className="board_container">
-            {displayCards}
+            {/* {cardData} */}
+            {/* was this before axios call {displayCards} */}
             <CardList cards={cardData} onDeleteCard={deleteCard}></CardList>
 
             <div className="new_card">
               <h3> Create a New Card</h3>
-              <NewCardForm onUpdateCardData={updateCardData} />
+              <NewCardForm onUpdateCardData={addCard} />
             </div>
           </div>
         )}
@@ -209,5 +149,6 @@ function App() {
       </footer> */}
     </section>
   );
-}
+};
+
 export default App;
