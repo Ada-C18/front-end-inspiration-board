16ceae90c271e72f78f7e8180289e2821b21d8ce
Valentina Ruiz <v.zruiz06@gmail.com>
2023-01-17 10:29:50 -0800

    created a dropdown component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,14 @@
 // import CardList from './CardList'
+import Dropdown from './Dropdown'
 
 
 const Board = () => {
     return (
         <section>
+            {/* <CardList></CardList> */}
+            <section>
+                <Dropdown></Dropdown>
+            </section>
             <h2>Boards</h2>
         </section>
     )
