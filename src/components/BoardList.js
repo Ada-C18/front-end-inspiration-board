c5dac8f62bf49ce8a7f42b4f19eda42e5f11c04a
Nina <nina.sohn@gmail.com>
2023-01-18 13:35:55 -0800

    committing BoardList

--- src/components/BoardList.js
+++ src/components/BoardList.js
@@ -1,9 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
-=======
 /* import Board from "./Board.js"; */
-import "./BoardList.css"
->>>>>>> 2ef70f206caa315d33e039550e5620bff8793c30
+import "./BoardList.css";
 
 const boardList = ({ boardList, loadBoard }) => {
   const boardComponents = [];
@@ -16,13 +13,8 @@ const boardList = ({ boardList, loadBoard }) => {
   }
 
   return (
-<<<<<<< HEAD
-    <div>
-      <h3>Board List Component</h3>
-=======
-    <div className= "board-list">
+    <div className="board-list">
       <h3>~All Boards~</h3>
->>>>>>> 2ef70f206caa315d33e039550e5620bff8793c30
       <ul>{boardComponents}</ul>
     </div>
   );
