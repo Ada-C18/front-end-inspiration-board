c6479b438821fb1a72420f679baa1a53c3a457bf
Eve Stroud <evestroud89@gmail.com>
2023-01-18 11:03:29 -0500

    Add sort options

--- src/Components/SortSelector.js
+++ src/Components/SortSelector.js
@@ -4,11 +4,14 @@ const SortSelector = (props) => {
   return (
     <div id="sort-selector">
       {"Sort by: "}
-      <select>
-        <option></option>
+      <select name="sortBy" onChange={() => null}>
+        <option value="id">Time created</option>
+        <option value="likes">Likes</option>
+        <option value="message">Alphabetically</option>
       </select>
-      <select>
-        <option></option>
+      <select name="sortDirection" onChange={() => null}>
+        <option value="asc">Ascending</option>
+        <option value="desc">Descending</option>
       </select>
     </div>
   );
