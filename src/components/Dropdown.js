175bed2a5070d730780ca1afed2e72069e92cec3
Valentina Ruiz <v.zruiz06@gmail.com>
2023-01-18 08:10:36 -0800

    possible resave but working through dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,16 +1,15 @@
 // shows new boards as they come up. Be able to create a new option when a new board is created
 // ideally tackling this 1/18/2023 together
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+
 
 const Dropdown = ({ boardData }) => {
   // console.log(boardData, "boardData");
   const onChangeBoard = (event) => {
-    // const board_title = event.target.value
+    const board_title = event.target.value
     const board_id = event.target.selectedIndex
+    console.log(board_title)
     console.log(board_id)
     
-
   }
   return (
     <div>
