08815538f976e6b5ad125c97725c05553e3da507
Minh <minh.seikel@gmail.com>
2023-01-18 11:43:34 -0800

    Alyssa & Minh added PropTypes for Card and CardList

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,37 @@
-import { FaRegTrashAlt } from 'react-icons/fa';
-import { AiOutlineLike, AiFillLike } from 'react-icons/ai';
+import { FaRegTrashAlt } from "react-icons/fa";
+import { AiOutlineLike, AiFillLike } from "react-icons/ai";
+import PropTypes from "prop-types";
 
 const Card = ({ card, updateLikedCard, deleteCard }) => {
-
   const likeOrNot = card.likes_count ? <AiFillLike /> : <AiOutlineLike />;
 
   return (
     // updated from ul to section
     <section className="card">
       <h1>{card.message}</h1>
-      <div className='card-elements'>
-        <p onClick = {()=>{updateLikedCard(card.id, card.likes_count+1)}}>{likeOrNot} {card.likes_count}</p>
-        <p onClick = {()=>{deleteCard(card.id)}}><FaRegTrashAlt/></p>
+      <div className="card-elements">
+        <p
+          onClick={() => {
+            updateLikedCard(card.id, card.likes_count + 1);
+          }}
+        >
+          {likeOrNot} {card.likes_count}
+        </p>
+        <p
+          onClick={() => {
+            deleteCard(card.id);
+          }}
+        >
+          <FaRegTrashAlt />
+        </p>
       </div>
     </section>
   );
 };
 
+Card.propTypes = {
+  card: PropTypes.object.isRequired,
+  updateLikedCard: PropTypes.func.isRequired,
+  deleteCard: PropTypes.func.isRequired,
+};
 export default Card;
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,81 +1,87 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Card from "./Card";
-
+import PropTypes from "prop-types";
 
 const CardList = ({ board }) => {
   const [cardsList, setCardsList] = useState([]);
-  
+
   const getAllCards = () => {
     axios
-    .get(`${process.env.REACT_APP_BACKEND_URL}/boards/${board.id}/cards`)
-    .then((response) => {
-      console.log(response.data);
-      setCardsList(response.data.cards)
-    })
-    .catch( (error) => {
-      console.log(error);
-    });
-  }
+      .get(`${process.env.REACT_APP_BACKEND_URL}/boards/${board.id}/cards`)
+      .then((response) => {
+        console.log(response.data);
+        setCardsList(response.data.cards);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   useEffect(getAllCards, [board]);
-
 
   const updateLikedCard = (cardId, increasedLike) => {
     const newCardsList = [];
     axios
-    .patch(`${process.env.REACT_APP_BACKEND_URL}/cards/${cardId}/${increasedLike}`)
-    .then((response)=> {
-      for (const card of cardsList) {
-        if (card.id !== cardId) {
-          newCardsList.push(card);
-        } else {
-          const newCard = {
-            ...card,
-            likes_count: increasedLike,
-          };
-          newCardsList.push(newCard);
+      .patch(
+        `${process.env.REACT_APP_BACKEND_URL}/cards/${cardId}/${increasedLike}`
+      )
+      .then((response) => {
+        for (const card of cardsList) {
+          if (card.id !== cardId) {
+            newCardsList.push(card);
+          } else {
+            const newCard = {
+              ...card,
+              likes_count: increasedLike,
+            };
+            newCardsList.push(newCard);
+          }
         }
-      }
-      setCardsList(newCardsList);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+        setCardsList(newCardsList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   const deleteCard = (cardId) => {
-    axios.delete(`${process.env.REACT_APP_BACKEND_URL}/cards/${cardId}`)
-    .then(() => {
-      const newCardsList = []; 
-      for (const card of cardsList) {
-        if (card.id !== cardId) {
-          newCardsList.push(card);
+    axios
+      .delete(`${process.env.REACT_APP_BACKEND_URL}/cards/${cardId}`)
+      .then(() => {
+        const newCardsList = [];
+        for (const card of cardsList) {
+          if (card.id !== cardId) {
+            newCardsList.push(card);
+          }
         }
-      }
-      setCardsList(newCardsList);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+        setCardsList(newCardsList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-// Need to add key here???
+  // Need to add key here???
   const cardsComponent = cardsList.map((card) => {
     return (
-      <Card card={card} updateLikedCard={updateLikedCard} deleteCard={deleteCard} />
+      <Card
+        card={card}
+        updateLikedCard={updateLikedCard}
+        deleteCard={deleteCard}
+      />
     );
   });
-
 
   return (
     <section className="cardList-container">
       <h1>Cards for {board.title}</h1>
-      <div className="cardListInner">
-        {cardsComponent}
-      </div>
+      <div className="cardListInner">{cardsComponent}</div>
     </section>
   );
 };
+CardList.propTypes = {
+  board: PropTypes.object.isRequired,
 
+};
 export default CardList;
