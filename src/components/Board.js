5b8469729083e6d07d2df7785e2dd560ab21f6d8
lisafeng <lisafenglisa@yahoo.com>
2023-01-17 23:04:43 -0500

    undated some css files, updated NewBoardForm for submitting a board form, still working on update and delete.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,36 +1,54 @@
-import CardList from './CardList'
+import React, { useState } from 'react';
 import Dropdown from './Dropdown'
 import NewBoardForm from './NewBoardForm'
 import NewCardForm from './NewCardForm'
 import './Board.css'
 import Card from './Card'
-
+import axios from 'axios';
 
 
 const Board = () => {
-    return (
-        <section className='board-container'>
-            <section className='input-section'>
-                <label>Choose Board to Display</label>
-                <Dropdown></Dropdown>
-                <label>Create a Board</label>
-                <NewBoardForm></NewBoardForm>
-                <label>Create a Card</label>
-                <NewCardForm></NewCardForm>
-                <h2>Board Title</h2>
-            </section>
-            <div className='card1'><Card></Card></div>
-            <div className='card2'><Card></Card></div>
-            <div className='card3'><Card></Card></div>
-            <div className='card4'><Card></Card></div>
-            <div className='card5'><Card></Card></div>
-            <div className='card6'><Card></Card></div>
-            <div className='card7'><Card></Card></div>
-            <div className='card8'><Card></Card></div>
+  const [boardData, setBoardData] = useState([]);
 
-            {/* <CardList></CardList> */}
-        </section>
-    )
+  const onBoardSubmit = (newBoardData) => {
+    axios.post('https://rykaliva.herokuapp.com/boards', newBoardData)
+    .then((response) => {
+      const newBoards = [...boardData];
+      newBoards.push({
+        id: response.data.id,
+        title: response.data.title,
+        owner: response.data.owner,
+        ...boardData
+      });
+
+      setBoardData(newBoards);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
+  return (
+    <section className='board-container'>
+      <section className='input-section'>
+        <label>Choose Board to Display</label>
+        <Dropdown></Dropdown>
+        <label>Create a Board</label>
+        <NewBoardForm onBoardSubmit={onBoardSubmit}></NewBoardForm>
+        <label>Create a Card</label>
+        <NewCardForm></NewCardForm>
+        <h2>Board Title</h2>
+      </section>
+      <div className='card1'><Card></Card></div>
+      <div className='card2'><Card></Card></div>
+      <div className='card3'><Card></Card></div>
+      <div className='card4'><Card></Card></div>
+      <div className='card5'><Card></Card></div>
+      <div className='card6'><Card></Card></div>
+      <div className='card7'><Card></Card></div>
+      <div className='card8'><Card></Card></div>
+    </section>
+  )
 }
 
 export default Board;
