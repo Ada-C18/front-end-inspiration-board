9245e42f713dd29fe7a0b20e6e45166a3cf4844d
SUZANNE ONEILL <SUZPONEILL@GMAIL.COM>
2023-01-17 21:06:49 -0800

    Added 40 character limit logic and style to new card form

--- front-end-inspiration-board/src/components/NewCardForm.js
+++ front-end-inspiration-board/src/components/NewCardForm.js
@@ -7,13 +7,17 @@ const INITIAL_CARD_FORM_DATA = {
 
 const NewCardForm = (props) => {
   const [formData, setFormData] = useState(INITIAL_CARD_FORM_DATA);
+  const [charCount, setCharCount] = useState(0);
 
   const handleChange = (e) => {
-    const newFormData = {
-      ...formData,
-      [e.target.name]: e.target.value,
-    };
-    setFormData(newFormData);
+    if (e.target.value.length <= 40) {
+      const newFormData = {
+        ...formData,
+        [e.target.name]: e.target.value,
+      };
+      setFormData(newFormData);
+      setCharCount(e.target.value.length);
+    }
   };
 
   const handleNewCardSubmit = (e) => {
@@ -22,6 +26,8 @@ const NewCardForm = (props) => {
     setFormData(INITIAL_CARD_FORM_DATA);
     props.closeDialogCallBackFunc();
   };
+
+  let messageValidity = charCount >= 40 ? "invalid" : "valid";
 
   return (
     <div className="CreateNewCard">
@@ -34,12 +40,14 @@ const NewCardForm = (props) => {
           Enter Your Message
         </label>
         <input
+          className={messageValidity}
           type="text"
           id="message"
           name="message"
           value={formData.message}
           onChange={handleChange}
         />
+        <h4>Remaining Characters: {40 - charCount}</h4>
         <input id="submitButton" type="submit" value="Add Card" />
       </form>
     </div>
