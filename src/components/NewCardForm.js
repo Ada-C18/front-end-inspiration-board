948cb9274fec0e2eb6befd0f02d59e620b7f0bcd
Selam Chaka <selblessed@gmail.com>
2023-01-18 09:23:48 -0500

    adding error handling

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -5,6 +5,7 @@ const NewCardForm = (props) => {
     message: "",
   });
 
+
   const onMessageChange = (event) => {
     setFormFields({
       ...formFields,
@@ -24,19 +25,29 @@ const NewCardForm = (props) => {
     });
   };
 
+  // const handleInput= (e) => {
+  //   if (e.target.value.length > 40){
+  //     setDisabled(true)
+  //   }else{
+  //     setDisabled(false)
+  //   }
+  // }
+
   return (
     <form onSubmit={FormSubmit}>
       <div>
         <label htmlFor="Message">Message:</label>
         <input
           name="Message"
+          // maxLength={40}
           value={formFields.message}
           onChange={onMessageChange}
+          // className={formFields.message.length > 40? 'max_length_input': ''}
         />
       </div>
       <p> Preview - </p>
       <p>{formFields.message}</p>
-      <input type="submit" value="Submit New Card" />
+      <input type="submit" value="Submit" disabled={formFields.message.length > 40} />
     </form>
   );
 };
