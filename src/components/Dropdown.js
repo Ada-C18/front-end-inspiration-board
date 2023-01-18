6af0d5d0f26520cef5a71cf3f0dc819ebad10751
lisafeng <lisafenglisa@yahoo.com>
2023-01-18 11:45:25 -0500

    not changing anything, just for a pull

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,5 @@
 // shows new boards as they come up. Be able to create a new option when a new board is created
 // ideally tackling this 1/18/2023 together
-import React, { useEffect, useState } from "react";
-import axios from "axios";
 
 const Dropdown = ({ boardData }) => {
   console.log(boardData, "boardData");
