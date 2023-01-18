ca4c41bf00b39df0ee576fdd277ec61ac9d7ba31
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-01-18 08:03:38 -0800

    fixed merge errors

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,36 +1,4 @@
-<<<<<<< HEAD
 import React from 'react';
-=======
-import React from "react";
-import PropTypes from "prop-types";
-import NewBoardForm from "./NewBoardForm";
-
-// const Board = (props) => {
-//     const boardComponents = props.entries.map((board) => {
-//         return (
-//             <div>
-//                 <NewBoardForm
-//                     title={board.title}
-//                 ></NewBoardForm>
-//             </div>
-//         );
-//     })
-
-//     return (
-//         <div onClick={() => props.onBoardSelect(props.board)}>
-//             {boardComponents}
-//         </div>
-//     );
-// };
-
-// Board.propTypes = {
-//     entries: PropTypes.arrayOf(PropTypes.shape({
-//         id: PropTypes.number.isRequired,
-//         board: PropTypes.string.isRequired,
-//     })),
-//     onBoardSelect: PropTypes.func.isRequired
-// };
->>>>>>> 9c4b77691a1ac2340941acbe6930077e1ca7db20
 
 const Board = (props) => {
   console.log(props);
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 
 const Card = (props) => {
 
@@ -13,59 +12,6 @@ const Card = (props) => {
         </ul>
     </div>
     );
-=======
-import PropTypes from "prop-types";
-import NewCardForm from "./NewCardForm";
-
-const Card = (props) => {
-  const cardMessage = props.entries.map((card, index) => {
-    return (
-      <p>
-        <NewCardForm message={card.message}></NewCardForm>
-      </p>
-    );
-  });
-
-  const cardLikes = props.entries.map((card, index) => {
-    return (
-      <p>
-        <NewCardForm likes={card.likes}></NewCardForm>
-      </p>
-    );
-  });
-
-  return (
-    <div>
-      {cardMessage}
-      <ul>
-        <li>{cardLikes}</li>
-        <li>
-          <p onClick={() => props.onLikeClick(props.card)}>+💖</p>
-        </li>
-        <li>
-          <p
-            className="card__delete"
-            onClick={() => props.deleteCardItem(props.card)}
-          >
-            Delete Card
-          </p>
-        </li>
-      </ul>
-    </div>
-  );
-};
-
-Card.propTypes = {
-  entries: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      message: PropTypes.string.isRequired,
-      likes: PropTypes.number.isRequired,
-    })
-  ),
-  onLikeClick: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired,
->>>>>>> 9c4b77691a1ac2340941acbe6930077e1ca7db20
 };
 
 export default Card;
