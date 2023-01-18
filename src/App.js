9c30e80c8a28bc3993017e90b07ec798a8f3990e
Rahel Kemal <rahelskemal@gmail.com>
2023-01-18 11:44:04 -0800

    added edits to app.js

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ const url = 'http://127.0.0.1:5000'
 
 function App() {
 
+  const [cardsData, setCardsData] = useState([]);
+
   const [boardsData, setBoardsData] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState({
     title: '',
-    owner: '',
+    author: '',
     board_id: null
   },[]);
+
+  console.log(selectedBoard)
 
   useEffect(() => {
     axios.get(`${url}/boards`, {}).then((response) => {
@@ -24,11 +28,22 @@ function App() {
     });
   }, []);
 
+  const onBoardSelect = (board) => {
+    axios.get(`${url}/boards/${board.board_id}/cards`).then((response) => {
+      setCardsData(response.data);
+  }).catch((error) => {
+      console.log('Error: Couldn\'t get all cards', error)
+      alert('Couldn\'t get all cards')
+  });
+  }
+
+
   const selectBoard = (board) => {
     setSelectedBoard(board)
   };
 
   const boardsElements = boardsData.map((board) => {
+    console.log(board)
     return (
       <li>
         <Board board={board} onBoardSelect={selectBoard}></Board>
@@ -40,15 +55,19 @@ function App() {
   const onBoardSubmit = (newBoard) => {
     console.log(newBoard)
     axios.post(`${url}/boards`, newBoard).then((response) => {
-      console.log('Response:', response.data.board);
+      console.log('Response:', response.data);
       const boards = [...boardsData];
-      boards.push(response.data.board);
+      boards.push(response.data);
       setBoardsData(boards);
     }).catch((error) => {
       console.log('Error: Couldn\'t create new board', error);
       alert('Couldn\'t create new board')
     })
   }
+
+// const selectOneBoard = (board) = {
+//   axios.get(`${url}/boards/<board_id>`)
+// }
 
   const [isBoardFormVisible, setIsBoardFormVisible] = useState(true);
 
@@ -77,11 +96,11 @@ function App() {
     //call the api endpoints here
     //onBoardSubmit={handleBoardSubmit}
   }
-    const handleBoardSubmit=(data)=>{
+  const handleBoardSubmit=(data)=>{
       //console.log("data",data)
       //call the api endpoints here
       
-    }
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -108,13 +127,17 @@ function App() {
             </ul>
           </section>
           <section>
-            <h2>Selected Board</h2>
-            {selectedBoard.board_id ? <CardList board={selectedBoard}></CardList> : ''}
+            <h2>Selected Board</h2> 
+
+            {/* <Board> {selectedBoard.board_id} ? `${selectedBoard.title} - ${selectedBoard.author}` </Board> */}
+            {/* <p>{selectBoard.board_id ? `${selectBoard.title} - ${selectBoard.author}` : 'Select a Board from the Board List!'}</p> */}
+    
           </section>
+          {onBoardSelect.board_id ? <Board board={onBoardSelect}></Board> : ''}
         </div>
         
     </div>
   );
-}
 
+  };
 export default App;
