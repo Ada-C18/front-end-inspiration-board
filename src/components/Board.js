7000d2528c63a580b028cf7a4687414e1f73153c
lisafeng <lisafenglisa@yahoo.com>
2023-01-18 09:31:29 -0500

    updated new board form and drowdown

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,54 +1,70 @@
-import React, { useState } from 'react';
-import Dropdown from './Dropdown'
-import NewBoardForm from './NewBoardForm'
-import NewCardForm from './NewCardForm'
-import './Board.css'
-import Card from './Card'
-import axios from 'axios';
-
+import React, { useEffect, useState } from "react";
+import Dropdown from "./Dropdown";
+import NewBoardForm from "./NewBoardForm";
+import NewCardForm from "./NewCardForm";
+import "./Board.css";
+import Card from "./Card";
+import axios from "axios";
 
 const Board = () => {
   const [boardData, setBoardData] = useState([]);
 
   const onBoardSubmit = (newBoardData) => {
-    axios.post('https://rykaliva.herokuapp.com/boards', newBoardData)
-    .then((response) => {
-      const newBoards = [...boardData];
-      newBoards.push({
-        id: response.data.id,
-        title: response.data.title,
-        owner: response.data.owner,
-        ...boardData
+    axios
+      .post("https://rykaliva.herokuapp.com/boards", newBoardData)
+      .then(() => {
+        axios.get("https://rykaliva.herokuapp.com/boards").then((response) => {
+          setBoardData(response.data);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-
-      setBoardData(newBoards);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
   };
 
+  useEffect(() => {
+    axios.get("https://rykaliva.herokuapp.com/boards").then((response) => {
+      setBoardData(response.data);
+    });
+  }, []);
+
   return (
-    <section className='board-container'>
-      <section className='input-section'>
+    <section className="board-container">
+      <section className="input-section">
         <label>Choose Board to Display</label>
-        <Dropdown></Dropdown>
+        <Dropdown boardData={boardData}></Dropdown>
         <label>Create a Board</label>
         <NewBoardForm onBoardSubmit={onBoardSubmit}></NewBoardForm>
         <label>Create a Card</label>
         <NewCardForm></NewCardForm>
         <h2>Board Title</h2>
       </section>
-      <div className='card1'><Card></Card></div>
-      <div className='card2'><Card></Card></div>
-      <div className='card3'><Card></Card></div>
-      <div className='card4'><Card></Card></div>
-      <div className='card5'><Card></Card></div>
-      <div className='card6'><Card></Card></div>
-      <div className='card7'><Card></Card></div>
-      <div className='card8'><Card></Card></div>
+      <div className="card1">
+        <Card></Card>
+      </div>
+      <div className="card2">
+        <Card></Card>
+      </div>
+      <div className="card3">
+        <Card></Card>
+      </div>
+      <div className="card4">
+        <Card></Card>
+      </div>
+      <div className="card5">
+        <Card></Card>
+      </div>
+      <div className="card6">
+        <Card></Card>
+      </div>
+      <div className="card7">
+        <Card></Card>
+      </div>
+      <div className="card8">
+        <Card></Card>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Board;
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,17 +1,19 @@
 // shows new boards as they come up. Be able to create a new option when a new board is created
 // ideally tackling this 1/18/2023 together
-const Dropdown = () => {
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Dropdown = ({ boardData }) => {
+  console.log(boardData, "boardData");
   return (
     <div>
       <select>
-        <option value="fruit">Fruit</option>
-        <option value="vegetable">Vegetable</option>
-        <option value="meat">Meat</option>
+        {boardData.map((opts, i) => (
+          <option key={i}>{opts.title}</option>
+        ))}
       </select>
     </div>
-    );
-  };
-    
-    
+  );
+};
 
 export default Dropdown;
