cc9fa8781d63fd8d66a2ea6f2b449d3c759845de
Sarah Sanborn <sanbornsarahn@gmail.com>
2023-01-19 11:25:42 -0800

    Updates to card css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,28 +3,29 @@ import { FaRegTrashAlt } from 'react-icons/fa';
 import { AiOutlineLike, AiFillLike } from 'react-icons/ai';
 
 const Card = ({ card, updateLikedCard, deleteCard }) => {
-  const likeOrNot = card.likes_count ? <AiFillLike className="like"/> : <AiOutlineLike className="like" />;
+  const likeOrNot = card.likes_count ? <AiFillLike /> : <AiOutlineLike />;
 
   return (
     // updated from ul to section
     <section className="card">
       <h1>{card.message}</h1>
-      <div className="card-elements">
-        <p
+      <ul className="card-elements">
+        <li
           onClick={() => {
             updateLikedCard(card.id, card.likes_count + 1);
           }}
         >
-          {likeOrNot} {card.likes_count}
-        </p>
-        <p
+          {likeOrNot}   
+        </li>
+        <li>{card.likes_count} </li>
+        <li className="trash"
           onClick={() => {
             deleteCard(card.id);
           }}
         >
-          <FaRegTrashAlt />
-        </p>
-      </div>
+          <FaRegTrashAlt className="trash"/>
+        </li>
+      </ul>
     </section>
   );
 };
