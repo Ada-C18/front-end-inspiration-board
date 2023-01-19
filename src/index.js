6044ad113ba9a11ef7c8aa3ba4114f6b42bfe37b
Eve Stroud <evestroud89@gmail.com>
2023-01-19 11:13:47 -0500

    Fix issue in src/index.js

--- src/index.js
+++ src/index.js
@@ -11,22 +11,6 @@ root.render(
   </React.StrictMode>
 );
 
-const errMsg = document.querySelector("#err-msg");
-const msgInput = errMsg.querySelector("textarea");
-
-const onInputChange = () => {
-  if (msgInput.value.length === 0 || msgInput.value.length > 40) {
-    errMsg.className = "invalid-input";
-  } else {
-    errMsg.className = "hide-err-msg";
-  }
-};
-
-const registerEventHandlers = () => {
-  msgInput.addEventListener("change", onInputChange);
-};
-
-document.addEventListener("DOMContentLoaded", registerEventHandlers);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
