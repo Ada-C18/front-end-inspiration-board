5a188e06c5ccda9ad848673e198306fc35738a5e
Jaime Mitchell <JaimeMitchell100@gmail.com>
2023-01-19 16:53:11 -0500

    dropdown is now working

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,46 +3,6 @@ import PropTypes from 'prop-types';
 import Card from './Card';
 import './CardList.css';
 
-// const CardList = (props) => {
-//   const cards = (props) => {
-//     return props.cards.map((card) => {
-//       return (
-//         <Card
-//           key={card.cardId}
-//           cardId={card.cardId}
-//           boardId={card.boardId}
-//           message={card.message}
-//           likesCount={card.likesCount}
-//           onDeleteCard={props.onDeleteCard}
-//           onLikesCount={props.onLikesCount}
-//         />
-//       );
-//     });
-//   };
-//   return (
-//     <div>
-//       <h2>Card List</h2>
-//       <section className='CardList'>{cards(props)}</section>
-//     </div>
-//   );
-// };
-
-// CardList.propTypes = {
-//   cards: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       cardId: PropTypes.number.isRequired,
-//       boardId: PropTypes.number.isRequired,
-//       message: PropTypes.string.isRequired,
-//       likesCount: PropTypes.number.isRequired
-//     })
-//   ).isRequired,
-//   onLikesCount: PropTypes.func.isRequired,
-//   onDeleteCard: PropTypes.func.isRequired,
-// };
-// export default CardList;
-
-
-
 const CardList = (props) => {
   
   // const handleSortChange = (event) => {
