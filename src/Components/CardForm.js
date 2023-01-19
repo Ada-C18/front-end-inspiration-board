1376513d3a6fcf4474b87c4261bde2b7fb590ed9
Uche Audrey <uaudrey3@gmail.com>
2023-01-18 22:15:57 -0500

    --

--- src/Components/CardForm.js
+++ src/Components/CardForm.js
@@ -15,29 +15,42 @@ const CardForm = (props) => {
     setMessage("");
   };
 
+  // const errMsg = (message) => {
+  //   if (message.length === 0 || message.length > 40) {
+  //     return "invalid-input"
+  //   } else {
+  //     return "hide-err-msg"
+  //     ? "invalid-input"
+  //     : "hide-err-msg"
+  //   }
+  // }
+
   return props.visible ? (
     <article className="add-card form">
       <div id="new-card-close" onClick={props.toggleVisible}>
         x
       </div>
+      {/* <div className="main-form"> */}
       <h3>create new card</h3>
       <hr></hr>
       <form onSubmit={onCardSubmit} className="card-form">
-        <textarea /*className*/
+        <textarea
           type="text"
           placeholder="Message"
-          className="form-field card-form-field"
+          className={"form-field card-form-field"}
           name="message"
           value={message}
           onChange={onMessageChange}
           rows="3"
         ></textarea>
+        <p>Invalid message. Message must be less than 40 characters.</p>
         <input
           type="submit"
           className="submit-btn card-submit-btn"
           value="add card"
         />
       </form>
+      {/* </div> */}
     </article>
   ) : (
     <article className="card-form-invisible" onClick={props.toggleVisible}>
