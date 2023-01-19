55919eaca7eb1806e47c9c86a84f3370762e2481
Uche Audrey <uaudrey3@gmail.com>
2023-01-19 15:46:30 -0500

    updated CSS for SortSelector, Header, and delete board

--- src/index.js
+++ src/index.js
@@ -32,27 +32,6 @@ document.addEventListener("scroll", debounce(storeScroll), { passive: true });
 
 storeScroll();
 
-// const errMsg = document.querySelector("#err-msg");
-// const msgInput = document.getElementById("text-area");
-
-// const onInputChange = () => {
-//   const errMsg = document.querySelector("#err-msg");
-//   const msgInput = document.getElementById("text-area");
-//   console.log("testing");
-//   if (msgInput.value.length === 0 || msgInput.value.length > 40) {
-//     errMsg.className = "invalid-input";
-//   } else {
-//     errMsg.className = "hide-err-msg";
-//   }
-// };
-
-// const registerEventHandlers = () => {
-//   const msgInput = document.getElementById("text-area");
-//   msgInput.addEventListener("change", onInputChange);
-// };
-
-// document.addEventListener("DOMContentLoaded", registerEventHandlers);
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
