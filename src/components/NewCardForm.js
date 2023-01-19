64476b2e4695c5a7b5acd7509c431ebc320a453c
Carina <checarina@gmail.com>
2023-01-18 22:35:15 -0800

    soooo much CSS

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -1,55 +1,56 @@
 import { useState } from "react";
 
 const NewCardForm = ({ addCardCallback }) => {
-	// console.log("callling new card form");
+  // console.log("callling new card form");
 
-	const [cardData, setCardData] = useState({ message: "" });
+  const [cardData, setCardData] = useState({ message: "" });
 
-	const onMessageChange = (event) => {
-		setCardData({
-			// ...cardData,
-			message: event.target.value,
-		});
-		// error message if card message more than 40 chars
-		if (event.target.value.length > 40) {
-			window.alert("Message shouldn't exceed 40 characters");
-		}
-	};
+  const onMessageChange = (event) => {
+    setCardData({
+      // ...cardData,
+      message: event.target.value,
+    });
+    // error message if card message more than 40 chars
+    if (event.target.value.length > 40) {
+      window.alert("Message shouldn't exceed 40 characters");
+    }
+  };
 
-	// Add Card
-	const onCardFormSubmit = (event) => {
-		console.log("Calling onCardFormSubmit");
-		event.preventDefault();
+  // Add Card
+  const onCardFormSubmit = (event) => {
+    console.log("Calling onCardFormSubmit");
+    event.preventDefault();
 
-		addCardCallback({
-			message: cardData.message,
-		});
+    addCardCallback({
+      message: cardData.message,
+    });
 
-		setCardData({ message: "" });
-	};
+    setCardData({ message: "" });
+  };
 
-	const isSubmitDisabled =
-		cardData.message === "" || cardData.message.length > 40;
+  const isSubmitDisabled =
+    cardData.message === "" || cardData.message.length > 40;
 
-	return (
-		<>
-			<form onSubmit={onCardFormSubmit}>
-				<label htmlFor="message">Message</label>
-				<input
-					type="string"
-					name="message"
-					id="message"
-					value={cardData.message}
-					onChange={onMessageChange}
-				/>
-				<p>Preview: {cardData.message}</p>
+  return (
+    <div className="form">
+      <h2>Create New Card</h2>
+      <form onSubmit={onCardFormSubmit}>
+        <label htmlFor="message">Message</label>
+        <input
+          type="string"
+          name="message"
+          id="message"
+          value={cardData.message}
+          onChange={onMessageChange}
+        />
+        <p>Preview: {cardData.message}</p>
 
-				<button type="submit" disabled={isSubmitDisabled}>
-					Submit
-				</button>
-			</form>
-		</>
-	);
+        <button type="submit" disabled={isSubmitDisabled}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default NewCardForm;
