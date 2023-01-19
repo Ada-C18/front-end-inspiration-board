f1343dedd6bf44e2913f4d480262a45ba61f4db2
Nicole Mejia <snicolemejia@gmail.com>
2023-01-19 11:42:42 -0500

    added styling to cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,8 +5,9 @@ import "./Card.css";
 const Card = ({ id, message, likeCount }) => {
     return (
         <div className="card__div">
-            <span>{message}</span>
-            {/* <span>{likeCount}</span> */}
+            <h3 className="card-message">{message}</h3>
+            <h4 className="card-like-count"> + {likeCount}</h4>
+            <button className="delete-card__button">Delete Card</button>
         </div>
     );
     };
