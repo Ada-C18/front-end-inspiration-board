d20eecf6b408ae4be30632c6f5fc2dd1abb342ad
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-01-19 08:49:21 -0800

    fixing buildpack

--- src/App.js
+++ src/App.js
@@ -56,6 +56,7 @@ const App = () => {
     setBoardFormVisibility(!boardFormVisibility);
   };
 
+  console.log('Deployment')
   // const deleteAllData = () => {
   //   if (window.confirm("Are you really sure you want to delete everything?")) {
   //     axios
