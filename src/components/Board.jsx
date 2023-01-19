8b5704d4ee5a96c4e8f6362af2e409bde8dd6b57
Steffanie Lopez <lopezsteffanie8@gmail.com>
2023-01-19 10:06:30 -0800

    trying to deploy

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Board = (props) => {
-  console.log("hi", props.board.title);
+  // console.log("hi", props.board.title);
   return (
     <div onClick={() => props.onBoardSelect(props.board)}>
       <li>
