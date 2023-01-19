3fafded36f6e9a8756099bf2d06027cdcc4888b3
Uche Audrey <uaudrey3@gmail.com>
2023-01-19 10:29:30 -0500

    pause js

--- src/index.js
+++ src/index.js
@@ -11,22 +11,22 @@ root.render(
   </React.StrictMode>
 );
 
-const errMsg = document.querySelector("#err-msg");
-const msgInput = errMsg.querySelector("textarea");
+// const errMsg = document.querySelector("#err-msg");
+// const msgInput = errMsg.querySelector("textarea");
 
-const onInputChange = () => {
-  if (msgInput.value.length === 0 || msgInput.value.length > 40) {
-    errMsg.className = "invalid-input";
-  } else {
-    errMsg.className = "hide-err-msg";
-  }
-};
+// const onInputChange = () => {
+//   if (msgInput.value.length === 0 || msgInput.value.length > 40) {
+//     errMsg.className = "invalid-input";
+//   } else {
+//     errMsg.className = "hide-err-msg";
+//   }
+// };
 
-const registerEventHandlers = () => {
-  msgInput.addEventListener("change", onInputChange);
-};
+// const registerEventHandlers = () => {
+//   msgInput.addEventListener("change", onInputChange);
+// };
 
-document.addEventListener("DOMContentLoaded", registerEventHandlers);
+// document.addEventListener("DOMContentLoaded", registerEventHandlers);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
