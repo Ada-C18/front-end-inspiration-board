45b3e903a78c40e2fe929aa498a1c479c7e68ef3
Liat <liathirsh@gmail.com>
2023-01-19 10:23:03 -0500

    Able to sort alphabetically, by likes, by ID

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,24 +1,89 @@
 import "./CardList.css";
 import Card from "./Card";
 import PropTypes from "prop-types";
-import axios from "axios";
+import { useState } from "react";
 
 const CardList = (props) => {
   const cards = props.cards;
+
+  const [sortedCards, setSortedCards] = useState(cards);
+
   return (
-    <div className="card_container">
-      {cards.map((card, id) => (
-        <Card
-          id={card.id}
-          likes={card.likes}
-          message={card.message}
-          key={id}
-          onDeleteCard={props.onDeleteCard}
-        />
-      ))}
+    <div>
+      <button
+        onClick={() => {
+          setSortedCards(
+            [...cards].sort((a, b) => {
+              // if return value is negative, a comes first
+              const aMessage = a.message.toLowerCase();
+              const bMessage = b.message.toLowerCase();
+              if (aMessage < bMessage) {
+                return -1;
+              } else if (bMessage < aMessage) {
+                return 1;
+              } else {
+                return 0;
+              }
+            })
+          );
+        }}
+      >
+        Sort Alphabetically
+      </button>
+      <button
+        onClick={() => {
+          setSortedCards(
+            [...cards].sort((a, b) => {
+              // if return value is negative, a comes first
+              if (a.likes < b.likes) {
+                return -1;
+              } else if (b.likes < a.likes) {
+                return 1;
+              } else {
+                return 0;
+              }
+            })
+          );
+        }}
+      >
+        {" "}
+        Sort by Likes{" "}
+      </button>
+      <button
+        onClick={() => {
+          setSortedCards(
+            [...cards].sort((a, b) => {
+              // if return value is negative, a comes first
+              if (a.id < b.id) {
+                return -1;
+              } else if (b.id < a.id) {
+                return 1;
+              } else {
+                return 0;
+              }
+            })
+          );
+        }}
+      >
+        {" "}
+        Sort by ID{" "}
+      </button>
+
+      <div className="card_container">
+        {sortedCards.map((card, id) => (
+          <Card
+            id={card.id}
+            likes={card.likes}
+            message={card.message}
+            key={id}
+            onDeleteCard={props.onDeleteCard}
+          />
+        ))}
+      </div>
     </div>
   );
 };
+
 CardList.propTypes = {
   cards: PropTypes.arrayOf(
     PropTypes.shape({
