1e785937182cf7f84bb63ab5f90b87e3d10fd2a6
Svetlana <svetikyss89@gmail.com>
2023-01-20 06:02:20 -0800

    finish style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 
 const Card = (props) => {
-    return (
-        <div className="container">
-        {props.card.message}
-        <ul className="content">
-            <li>{props.card.likes_count}</li>
-            <li>
-            <p onClick={() => props.onLikeClick(props.card)}>+💖</p>
-            </li>
-            <li>
-            <p
-                className="card-item__delete"
-                onClick={() => props.deleteCard(props.card)}
-            >
-                Delete Card
-            </p>
-            </li>
-        </ul>
+  return (
+    <div className="container">
+      <div className="card-item__message">{props.card.message}</div>
+      <div className="content">
+        <div className="second">
+          <section className="ac-footer">
+            <div className="ac-footer-container ac-footer-brand">
+              <div onClick={() => props.onLikeClick(props.card)}>
+                <span class="ac-icon ac-icon-love-dark"></span>
+              </div>{" "}
+            </div>{" "}
+            <span id="number">{props.card.likes_count}</span>
+          </section>
         </div>
-    );
+
+        <div className="last">
+          <p onClick={() => props.deleteCard(props.card)}>
+            <span class="trash">
+              <span></span>
+              <i></i>
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,95 +4,95 @@ import Card from "./Card";
 import NewCardForm from "./NewCardForm";
 
 const CardList = (props) => {
-    const [cardData, setCardData] = useState([]);
+  const [cardData, setCardData] = useState([]);
 
-    useEffect(() => {
-        axios
-        .get(
-            `${process.env.REACT_APP_BACKEND_URL}/boards/${props.board.board_id}/cards`
-        )
-        .then((response) => {
-            // console.log('Response', response)
-            setCardData(response.data.cards);
-        })
-        .catch((error) => {
-            console.log("Error:", error);
-            alert("Couldn't get cards for this board.");
-        });
-    }, [props.board]);
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.REACT_APP_BACKEND_URL}/boards/${props.board.board_id}/cards`
+      )
+      .then((response) => {
+        // console.log('Response', response)
+        setCardData(response.data.cards);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        alert("Couldn't get cards for this board.");
+      });
+  }, [props.board]);
 
-    const deleteCard = (card) => {
-        axios
-        .delete(`${process.env.REACT_APP_BACKEND_URL}/cards/${card.card_id}`)
-        .then((response) => {
-            const newCardData = cardData.filter((currentCard) => {
-            return currentCard.id !== card.id;
+  const deleteCard = (card) => {
+    axios
+      .delete(`${process.env.REACT_APP_BACKEND_URL}/cards/${card.card_id}`)
+      .then((response) => {
+        const newCardData = cardData.filter((currentCard) => {
+          return currentCard.id !== card.id;
         });
         setCardData(newCardData);
         window.location.reload();
-        }).catch((error) => {
-            console.log("Error:", error);
-            alert("Couldn't delete the card.");
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        alert("Couldn't delete the card.");
+      });
+  };
+
+  const onLikeClick = (card) => {
+    axios
+      .put(`${process.env.REACT_APP_BACKEND_URL}/cards/${card.card_id}/likes`)
+      .then((response) => {
+        const newCardData = cardData.map((currentCard) => {
+          return currentCard.card_id !== card.card_id
+            ? currentCard
+            : { ...card, likes_count: card.likes_count + 1 };
         });
-    };
+        setCardData(newCardData);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        alert("Couldn't like the card.");
+      });
+  };
 
-    const onLikeClick = (card) => {
-        axios
-        .put(`${process.env.REACT_APP_BACKEND_URL}/cards/${card.card_id}/likes`)
-        .then((response) => {
-            const newCardData = cardData.map((currentCard) => {
-            return currentCard.card_id !== card.card_id
-                ? currentCard
-                : { ...card, likes_count: card.likes_count + 1 };
-            });
-            setCardData(newCardData);
-        })
-        .catch((error) => {
-            console.log("Error:", error);
-            alert("Couldn't like the card.");
-        });
-    };
-
-    const cardElements = cardData.map((card) => {
-        return (
-        <Card
-            card={card}
-            onLikeClick={onLikeClick}
-            deleteCard={deleteCard}
-        ></Card>
-        );
-    });
-
-    const postNewCard = (message) => {
-        axios
-        .post(
-            `${process.env.REACT_APP_BACKEND_URL}/boards/${props.board.board_id}/cards`,
-            { message }
-        )
-        .then((response) => {
-            const cards = [...cardData];
-            cards.push(response.data.card);
-            setCardData(cards);
-            window.location.reload();
-        }).catch((error) => {
-            console.log('Error:', error);
-            alert('Couldn\'t create a new card.');
-        });
-    };
-
+  const cardElements = cardData.map((card) => {
     return (
-        <section className='cards__container'>
-        <section>
-            <h2>Cards for {props.board.title}</h2>
-            <div className='card-data'>
-            {cardElements}
-            </div>
-        </section>
-        <NewCardForm
-            postNewCard={postNewCard}
-        ></NewCardForm>
-        </section>
-    )
+      <Card
+        card={card}
+        onLikeClick={onLikeClick}
+        deleteCard={deleteCard}
+      ></Card>
+    );
+  });
+
+  const postNewCard = (message) => {
+    axios
+      .post(
+        `${process.env.REACT_APP_BACKEND_URL}/boards/${props.board.board_id}/cards`,
+        { message }
+      )
+      .then((response) => {
+        const cards = [...cardData];
+        cards.push(response.data.card);
+        setCardData(cards);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+        alert("Couldn't create a new card.");
+      });
+  };
+
+  return (
+    <section className="cards__container">
+      <section>
+        <h2>
+          Cards for <h4 className="text-effect h4">{props.board.title}</h4>
+        </h2>
+        <div className="card-items__container">{cardElements}</div>
+      </section>
+      <NewCardForm postNewCard={postNewCard}></NewCardForm>
+    </section>
+  );
 };
 
 export default CardList;
