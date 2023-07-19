347bd9b751dcdf775ffb936c168a7a4cc34efd6e
Amber Shay <mashaad21@gmail.com>
2023-07-19 10:09:39 -0600

    removed header file

--- src/App.js
+++ src/App.js
@@ -7,19 +7,8 @@ function App() {
   const baseURLBoards = 'http://127.0.0.1:5000/boards'
   const [boardData, setBoardData] = useState([]);
 
-  useEffect(() => {
-    axios
-    .get(baseURLBoards)
-    .then((res) => {
-      console.log(res.data)
-      setBoardData(res.data)
-    })
-    .catch((err) => console.log(err))
-  }, []);
-
   return (
     <main>
-      <Header />
       <BoardList boardData={boardData} />
     </main>
   )
