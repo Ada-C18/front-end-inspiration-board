069075abe6aa5b8b380cf39be0af1f3a5bc79f51
Angelica <angelica.yee57@gmail.com>
2023-07-19 13:34:31 -0500

    f added the increment counter and corresponding cardstate into App

--- src/App.js
+++ src/App.js
@@ -6,10 +6,24 @@ import axios from 'axios';
 function App() {
   const baseURLBoards = 'http://127.0.0.1:5000/boards'
   const [boardData, setBoardData] = useState([]);
-
+  const [cardData, setCardData] = useState([]);
+  const incrementCounter = (id) => {
+    setCardData((prev) => {
+      return prev.map((entry) => {
+        if (id === entry.id) {
+          return {
+            ...entry,
+            likes_count: entry.likes_count + 1,
+          };
+        } else {
+          return entry;
+        }
+      });
+    });
+  };
   return (
     <main>
-      <BoardList boardData={boardData} />
+      <BoardList boardData={boardData} cardData={cardData} incrementCounter={incrementCounter} />
     </main>
   )
 };
