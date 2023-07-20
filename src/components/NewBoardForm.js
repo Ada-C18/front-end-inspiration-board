fc9867ea8367cd0df8249271d5bbceec459d848a
Amber Shay <mashaad21@gmail.com>
2023-07-20 11:07:06 -0600

    fixed variable naming issues

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -8,7 +8,7 @@ const NewBoardForm = (props) => {
 
     const handleSubmitBoard = (event) => {
         event.preventDefault();
-        props.onSubmit({
+        props.submitForm({
             title: title,
             owner: owner,
         });
